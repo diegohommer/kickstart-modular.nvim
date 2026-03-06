46d06eff6fddb2addcb589f3fe9c3bc0a3fa748a
diegohommer <daugsteel@gmail.com>
2026-03-06 16:59:00 -0300

    feat: enable format on save for python black

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -19,7 +19,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, python = true, ruby = true }
+        local disable_filetypes = { c = true, cpp = true, ruby = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -31,14 +31,16 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'isort', 'black' },
+        python = { 'black' },
         c = { 'clang-format' },
         cpp = { 'clang-format' },
+        cs = { 'csharpier' },
         javascript = { 'prettier' },
         javascriptreact = { 'prettier' },
         typescript = { 'prettier' },
         typescriptreact = { 'prettier' },
         markdown = { 'prettier' },
+        json = { 'prettier' },
       },
       formatters = {
         black = {
